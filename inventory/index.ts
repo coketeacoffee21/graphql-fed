af4a71b7cff053905bad194b5938cb347611c334
Marco Cheung <marco.mh.cheung@gtomato.com>
2021-02-04 13:24:28 +0800

    setup

---
+++ inventory/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from "apollo-server";
+
+import InventoryResolver from "./resolver";
+import Product from "./product";
+import { resolveProductReference } from "./product-reference";
+import { buildFederatedSchema } from "../helpers/buildFederatedSchema";
+
+export async function listen(port: number): Promise<string> {
+  const schema = await buildFederatedSchema(
+    {
+      resolvers: [InventoryResolver],
+      orphanedTypes: [Product],
+    },
+    {
+      Product: { __resolveReference: resolveProductReference },
+    },
+  );
+
+  const server = new ApolloServer({
+    schema,
+    tracing: false,
+    playground: true,
+  });
+
+  const { url } = await server.listen({ port });
+  console.log(`Inventory service ready at ${url}`);
+
+  return url;
+}
